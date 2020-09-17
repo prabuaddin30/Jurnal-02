9d7f8b8b0573d429f133fcaeb8acb5bb2329582f
prabuaddin30 <prabuaddin@student.telkomuniversity.ac.id>
2020-09-17 21:35:14 +0700

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
-public class Hello World {
-    public static void main (String[] args)  {
-	    System.out.println("Hello World!");
-    }
+class HelloWorld{
+    public static void main(String args[]){
+	    System.out.println("Prabu Addin Almuhasibi");
+		System.out.println("NIM:D3IF-6706200063");
+		System.out.println("Kelas:44-03");
+	}
 }
